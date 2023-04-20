7960ebff36550cd6c593ad75c5c9259fba06ae48
imanujsharma99 <imanujsharma999@gmail.com>
2023-04-21 01:32:25 +0530

    bankLoginFrontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Login } from './components/login/Login';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+      
+  
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './contex/AuthProvider';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <>
+ <AuthProvider>
+ <App />
+ </AuthProvider>
+  
+ </>
+     
+
+);
+
+reportWebVitals();
